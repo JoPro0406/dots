c33d2f049b72b4eccd02b871909d3fd198e82fee
Jonas Seifert <55317573+JoPro0406@users.noreply.github.com>
2021-02-01 17:24:24 +0100

    updot

--- wsl/etc/nvim/lua/lsp/completion.lua
+++ wsl/etc/nvim/lua/lsp/completion.lua
@@ -1,7 +1,7 @@
 local lspconfig = require('lspconfig')
 local hv = require('helpful.vim')
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = false,
     virtual_text = false,
@@ -12,23 +12,23 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 local on_attach = function()
   -- Keybindings for LSPs
-  hv.noremap("n", "gd"   , "<cmd>lua vim.lsp.buf.definition()<CR>"      , { silent = true })
-  hv.noremap("n", "gh"   , "<cmd>lua vim.lsp.buf.hover()<CR>"           , { silent = true })
-  hv.noremap("n", "gD"   , "<cmd>lua vim.lsp.buf.implementation()<CR>"  , { silent = true })
-  hv.noremap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>"  , { silent = true })
-  hv.noremap("n", "1gD"  , "<cmd>lua vim.lsp.buf.type_definition()<CR>" , { silent = true })
-  hv.noremap("n", "gr"   , "<cmd>lua vim.lsp.buf.references()<CR>"      , { silent = true })
-  hv.noremap("n", "g0"   , "<cmd>lua vim.lsp.buf.document_symbol()<CR>" , { silent = true })
-  hv.noremap("n", "gW"   , "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", { silent = true })
+  hv.noremap('n', 'gd'   , '<cmd>lua vim.lsp.buf.definition()<CR>'      , { silent = true })
+  hv.noremap('n', 'gh'   , '<cmd>lua vim.lsp.buf.hover()<CR>'           , { silent = true })
+  hv.noremap('n', 'gD'   , '<cmd>lua vim.lsp.buf.implementation()<CR>'  , { silent = true })
+  hv.noremap('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>'  , { silent = true })
+  hv.noremap('n', '1gD'  , '<cmd>lua vim.lsp.buf.type_definition()<CR>' , { silent = true })
+  hv.noremap('n', 'gr'   , '<cmd>lua vim.lsp.buf.references()<CR>'      , { silent = true })
+  hv.noremap('n', 'g0'   , '<cmd>lua vim.lsp.buf.document_symbol()<CR>' , { silent = true })
+  hv.noremap('n', 'gW'   , '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { silent = true })
   require('completion').on_attach()
 end
 
 lspconfig.rust_analyzer.setup({ on_attach = on_attach })
 lspconfig.vimls.setup({ on_attach = on_attach  })
 local sumneko_root_path = vim.fn.stdpath('cache')..'/lsp/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+local sumneko_binary = sumneko_root_path..'/bin/Linux/lua-language-server'
 lspconfig.sumneko_lua.setup({
-  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua"  };
+  cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua'  };
   settings = {
     Lua = {
       runtime = {
--- wsl/etc/nvim/lua/options/plugins.lua
+++ wsl/etc/nvim/lua/options/plugins.lua
@@ -1,15 +1,15 @@
-local hv = require("helpful.vim")
+local hv = require('helpful.vim')
 
 hv.setoption({
-  completeopt = "menuone,noinsert,noselect"
+  completeopt = 'menuone,noinsert,noselect'
 })
 
 hv.setglobal({
   completion_chain_complete_list = {
     default = {
-      {complete_items = {"lsp", "snippet", "tabnine"}},
-      {mode = "<c-p>"},
-      {mode = "<c-n>"}
+      {complete_items = {'lsp', 'snippet', 'tabnine'}},
+      {mode = '<c-p>'},
+      {mode = '<c-n>'}
     }
   },
   ale_disable_lsp = 1,
@@ -17,63 +17,69 @@ hv.setglobal({
   -- ale_fixers   = { -- fix at bottom
   --   '*'        = { 'remove_trailing_lines', 'trim_whitespace' },
   -- },
+  ale_lint_on_text_changed = 'always',
 
   indentLine_setColors = 0,
   indentLine_enabled = 0,
   indentLine_leadingSpaceEnabled = 1,
-  indentLine_leadingSpaceChar = "·",
-  indentLine_fileTypeExclude = {"dashboard"},
+  indentLine_leadingSpaceChar = '·',
+  indentLine_fileTypeExclude = {'dashboard'},
 
   NERDSpaceDelims = 1,
   NERDCommentEmptyLines = 1,
   NERDTrimTrailingWhitespace = 1,
 
-  dashboard_default_header = "commicgirl15",
-  dashboard_default_executive = "telescope",
+  dashboard_default_header = 'commicgirl15',
+  dashboard_default_executive = 'telescope',
   dashboard_custom_shortcut = {
-    last_session = "SPC s l",
-    find_history = "SPC f h",
-    find_file = "SPC f f",
-    new_file = "SPC n f",
-    change_colorscheme = "SPC t c",
-    find_word = "SPC f a",
-    book_marks = "SPC f b"
+    last_session = 'SPC s l',
+    find_history = 'SPC f h',
+    find_file = 'SPC f f',
+    new_file = 'SPC n f',
+    change_colorscheme = 'SPC t c',
+    find_word = 'SPC f a',
+    book_marks = 'SPC f b'
   },
 
   lightline = {
-    colorscheme = "ayu_dark",
+    colorscheme = 'ayu_dark',
     active = {
-      left = {{"mode", "paste"}, {"gitbranch", "filename"}},
+      left = {{ 'mode', 'paste' }, { 'gitbranch', 'filename' }},
       right = {
-        {"lineinfo"},
-        {"liner_checking", "linter_errors", "linter_warnings", "linter_infos", "linter_ok"},
-        {"fileformat", "fileencoding", "filetype"}
+        { 'lineinfo' },
+        { 'liner_checking', 'linter_errors', 'linter_warnings', 'linter_infos', 'linter_ok' },
+        { 'fileformat', 'fileencoding', 'filetype' }
       }
     },
     tabline = {
-      left = {{"buffers"}}
+      left = {{ 'buffers' }},
+      right = {{ 'close' }}
     },
     component_function = {
-      gitbranch = "FugitiveHead"
+      gitbranch = 'FugitiveHead'
     },
     component_expand = {
-      buffers = "lightline#bufferline#buffers",
-      linter_checking = "lightline#ale#checking",
-      linter_infos = "lightline#ale#infos",
-      linter_warnings = "lightline#ale#warnings",
-      linter_errors = "lightline#ale#errors",
-      linter_ok = "lightline#ale#ok"
+      buffers = 'lightline#bufferline#buffers',
+      linter_checking = 'lightline#ale#checking',
+      linter_infos = 'lightline#ale#infos',
+      linter_warnings = 'lightline#ale#warnings',
+      linter_errors = 'lightline#ale#errors',
+      linter_ok = 'lightline#ale#ok'
     },
     component_type = {
-      buffers = "tabsel",
-      linter_checking = "right",
-      linter_infos = "right",
-      linter_warnings = "warning",
-      linter_errors = "error",
-      linter_ok = "right"
+      buffers = 'tabsel',
+      linter_checking = 'right',
+      linter_infos = 'right',
+      linter_warnings = 'warning',
+      linter_errors = 'error',
+      linter_ok = 'right'
     }
   }
 })
 vim.cmd([[let g:ale_fixers = { '*': [ 'remove_trailing_lines', 'trim_whitespace'  ] }]])
 vim.cmd([[autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2]])
+
+vim.cmd([[autocmd! User GoyoEnter Limelight]])
+vim.cmd([[autocmd! User GoyoLeave Limelight!]])
+
 require('colorizer').setup()
