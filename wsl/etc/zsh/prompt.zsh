a5c4648d5a96bc6fafe5af11e7fc09c8c178830d
Jonas Seifert <55317573+JoPro0406@users.noreply.github.com>
2021-02-03 14:17:13 +0100

    updot

--- wsl/etc/zsh/prompt.zsh
+++ wsl/etc/zsh/prompt.zsh
@@ -1,1 +1,37 @@
-PS1='%B%F{blue}%3~ | %b%f'
+dir() {
+  local CUTOFF=1
+  local IFS=/
+  local my_path=($(print -P '%~'))
+  local p
+  echo -n "%B%F{blue}"
+  for p in $my_path; do
+    echo -n "${s}${p[0,$CUTOFF]}"
+    local s=/
+  done
+  echo -n "${p:$CUTOFF}"
+  echo -n "%f%b"
+}
+
+git_status() {
+  local BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\s*\(.*\)/\1/')
+
+  if [ ! -z $BRANCH ]; then
+    echo -n "on %F{cyan}$BRANCH%f"
+    [ ! -z "$(git status --short)" ] && echo -n " %F{yellow}*%f"
+  fi
+}
+
+function _my_prompt() {
+  local exit_code="$?"
+  echo -n "%B%F{blue}╭─%f%b"
+  echo -n " $(dir)"
+  echo -n " $(git_status)"
+  if [ "$exit_code" != 0 ]; then
+    echo -n " %F{red}[%B$exit_code%b]%f"
+  fi
+  echo
+  echo -n "%B%F{blue}╰→ %f%b"
+}
+
+setopt prompt_subst
+PS1='$(_my_prompt)'
